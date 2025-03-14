2bcba764e9b6e6ea355a440ad2a5d55a8a61636b
somethingwithagun <iamunknownsubject@gmail.com>
2025-03-14 11:28:04 +0300

    test

--- Team28240/src/main/java/org/firstinspires/ftc/team28420/MainTeleOp.java
+++ Team28240/src/main/java/org/firstinspires/ftc/team28420/MainTeleOp.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.team28420.BigKostyl.BigKostyl;
 
-@TeleOp(name = "main teleop", group = "TeleOp")
+@TeleOp(name = "main teleop", 123group = "TeleOp")
 public class MainTeleOp extends LinearOpMode {
     public static Telemetry telem;
 
