6fc943f5f77b069d0d545cd70d51c400960f0368
user <iamunknownsubject@gmail.com>
2025-03-27 21:43:51 +0300

    extended joystick. Need to integrate into teleop

--- Team28240/src/main/java/org/firstinspires/ftc/team28420/module/Joystick.java
+++ Team28240/src/main/java/org/firstinspires/ftc/team28420/module/Joystick.java
@@ -12,17 +12,76 @@ public class Joystick {
         this.gamepad = gamepad;
     }
 
+    public boolean getLeftBumper() {
+        return gamepad.left_bumper;
+    }
+
+    public boolean getRightBumper() {
+        return gamepad.right_bumper;
+    }
+
+    public boolean getDPadUp() {
+        return gamepad.dpad_up;
+    }
+
+    public boolean getDpadDown() {
+        return gamepad.dpad_down;
+    }
+
+    public boolean getDPadLeft() {
+        return gamepad.dpad_left;
+    }
+
+    public boolean getDPadRight() {
+        return gamepad.dpad_right;
+    }
+
+    public boolean getTriangle() {
+        return gamepad.triangle;
+    }
+
+    public boolean getCircle() {
+        return gamepad.circle;
+    }
+
+    public boolean getCross() {
+        return gamepad.cross;
+    }
+
+    public boolean getSquare() {
+        return gamepad.square;
+    }
+
+    public double getLeftTrigger() {
+        float x = gamepad.left_trigger;
+        return x*x;
+    }
+    public double getRightTrigger() {
+        float x = gamepad.right_trigger;
+        return x*x;
+    }
+
     public Pos getLeftStickPos() {
+        float x = gamepad.left_stick_x, y = gamepad.left_stick_y;
+
+        x = ( x > 0 ? x*x : -x*x );
+        y = ( y > 0 ? y*y : -y*y );
+
         return new Pos(
-            Math.abs(gamepad.left_stick_x)>0.2?gamepad.left_stick_x*0.8:0,
-            Math.abs(gamepad.left_stick_y)>0.2?-gamepad.left_stick_y*0.8:0
+            Math.abs(x) > 0.1 ? x * 0.5 : 0,
+            Math.abs(y) > 0.1 ? -y * 0.5 : 0
         );
     }
 
     public Pos getRightStickPos() {
+        float x = gamepad.right_stick_x, y = gamepad.right_stick_y;
+
+        x = ( x > 0 ? x*x : -x*x );
+        y = ( y > 0 ? y*y : -y*y );
+
         return new Pos(
-                Math.abs(gamepad.right_stick_x)>0.2?gamepad.right_stick_x:0,
-                Math.abs(gamepad.right_stick_y)>0.2?-gamepad.right_stick_y:0
+                Math.abs(x) > 0.1 ? x * 0.2 : 0,
+                Math.abs(y) > 0.1 ? -y * 0.2 : 0
         );
     }
 
